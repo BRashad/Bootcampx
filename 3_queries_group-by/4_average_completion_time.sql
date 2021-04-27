0519bdf3362a85fb75ed20f7bef5dc819cd9f7e5
Rashad Bayram <rashadabayram.com>
2021-04-27 09:30:52 -0600

    final querry submitted

--- 3_queries_group-by/4_average_completion_time.sql
+++ 3_queries_group-by/4_average_completion_time.sql
@@ -1,6 +1,6 @@
-SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
+SELECT students.name as student, avg(assignments.duration) as average_assignment_duration
 FROM students
-JOIN assignment_submissions ON student_id = students.id
+JOIN assignments ON student_id = students.id
 WHERE end_date IS NULL
 GROUP BY student
 ORDER BY average_assignment_duration DESC;
--- 3_queries_group-by/5_low_average_completion_time.sql
+++ 3_queries_group-by/5_low_average_completion_time.sql
@@ -1,3 +1,8 @@
-SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments_seeds) as average_estimated_duration
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
 FROM students
-JOIN 
+JOIN assignment_submissions ON student_id = students.id
+JOIN assignments ON assignments.id = assignment_id
+WHERE end_date IS NULL
+GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration;
