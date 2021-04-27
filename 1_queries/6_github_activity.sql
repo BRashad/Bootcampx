3df76fcdf77157ea4742242cd372847ba44f0bae
Rashad Bayram <rashadabayram.com>
2021-04-26 18:22:10 -0600

    assignment duration tables created and query added

--- 1_queries/6_github_activity.sql
+++ 1_queries/6_github_activity.sql
@@ -2,3 +2,5 @@ SELECT name, email, phone
 FROM students
 WHERE github is NULL
 AND start_date IS NOT NULL
+
+
---
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -0,0 +1,2 @@
+SELECT total_amount FROM students JOIN assignment_submissions ON student_id = students.id
+WHERE name IN ('Ibrahim Schimmel') 
